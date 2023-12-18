8ca319db6e5d7fc8df395155346e98b7cff606c7
Moussa Clarke <moussaclarke@gmail.com>
2023-12-18 09:50:20 +0000

    chore: valet 4 driver

--- utils/FormeServerValetDriver.php
+++ utils/FormeServerValetDriver.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace Valet\Drivers\Custom;
+
+use Valet\Drivers\ValetDriver;
+
 class FormeServerValetDriver extends WordPressValetDriver
 {
     /**
@@ -47,7 +51,7 @@ class FormeServerValetDriver extends WordPressValetDriver
     {
         $_SERVER['SERVER_ADDR'] = '127.0.0.1';
         $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
-        
+
         return parent::frontControllerPath($sitePath . self::sitePathSuffix, $siteName, $uri);
     }
 }
